fb3b1d54f0779647b266173e14d2d8582f565e08
wesclaw <108541397+wesclaw@users.noreply.github.com>
2023-12-09 19:28:39 +0100

    added some more data for database

--- js/main.js
+++ js/main.js
@@ -5,8 +5,42 @@ const itemsList = document.querySelector('.item-list');
 
 let chatPromptList = []
 
-function generateTopIngredients(ingred) {
-  for(let i=0;i<21;i++){
+function generateTopIngredients(ingred) { 
+  for(let i=0;i<50;i++){
+    const ingred = document.createElement('div')
+    ingred.classList.add('ingred')
+
+    const p_el = document.createElement('p')
+    p_el.textContent = ingredients[i].name
+
+    const img_el = document.createElement('img')
+    img_el.classList.add('ingredImage')
+    img_el.src = ingredients[i].image
+
+    ingred.append(p_el)
+    ingred.append(img_el)
+
+    wrapperForBtns.append(ingred)
+
+    ////removing the ingred top item on click
+    ingred.addEventListener('click',(e)=>{
+      ingred.style.display = 'none'
+      addToList(p_el, img_el)
+    })
+  }
+  ///making the generate more btn/////
+    const ranBtn = document.createElement('button')
+    ranBtn.classList.add('ranBtn')
+    ranBtn.innerHTML = `Generate More <img class='ranBtnImg' src='otherImages/dice.png'></img>`
+    wrapperForBtns.insertBefore(ranBtn, ingred)
+
+    // random btn e listener
+    ranBtn.addEventListener('click', getRandomIngredients)
+}
+// 
+
+function getRandomIngredients() {
+  for(let i=0;i<ingredients.length;i++){
     const ingred = document.createElement('div')
     ingred.classList.add('ingred')
 
@@ -30,6 +64,7 @@ function generateTopIngredients(ingred) {
   }
 }
 
+// 
 
 function addToList(p_el, img_el) {
   const div_tag = document.createElement('div')
@@ -37,7 +72,6 @@ function addToList(p_el, img_el) {
   const x_tag = document.createElement('p')
   x_tag.classList.add('x_tag')
   x_tag.textContent = '\uD83D\uDDD9'
-
   
   div_tag.append(p_el)
   div_tag.append(img_el)
@@ -49,9 +83,7 @@ function addToList(p_el, img_el) {
   chatPromptList.push(p_el.textContent)
 
   console.log(chatPromptList)
-
 }
-
 
 function removeIngredients(e) {
   if(e.target.classList.contains('x_tag')){
@@ -60,10 +92,7 @@ function removeIngredients(e) {
   console.log(items)
 }
 
-
-
 generateTopIngredients()
-
 
 itemsList.addEventListener('click', removeIngredients)
 
