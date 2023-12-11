335d03ec33bdf1955bbebf6ff7ed25f4fa4c4007
wesclaw <108541397+wesclaw@users.noreply.github.com>
2023-12-11 21:52:55 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -32,79 +32,15 @@ function generateTopIngredients(ingred) {
     const ranBtn = document.createElement('button')
     ranBtn.classList.add('ranBtn')
     ranBtn.innerHTML = `Generate More <img class='ranBtnImg' src='otherImages/dice.png'></img>`
-    wrapperForBtns.insertBefore(ranBtn, ingred)
-
-    generateRandomIngred(ranBtn)
-    
+    wrapperForBtns.insertBefore(ranBtn, ingred)    
 }
-// 
-
-function generateRandomIngred(ranBtn) {
-  ranBtn.addEventListener('click', e => {
-    wrapperForBtns.innerHTML = '';
-    
-    const displayedIngredients = new Set(); // To track displayed ingredients
-
-    // Display the first 50 unique ingredients (or all if there are fewer than 50)
-    for (let i = 0; i < ingredients.length && displayedIngredients.size < 50; i++) {
-      const randomIndex = Math.floor(Math.random() * ingredients.length);
-
-      // Check if the ingredient has already been displayed
-      if (!displayedIngredients.has(randomIndex)) {
-        displayedIngredients.add(randomIndex);
-
-        const ingred = document.createElement('div');
-        ingred.classList.add('ingred');
-
-        const p_el = document.createElement('p');
-        p_el.textContent = ingredients[randomIndex].name;
-
-        const img_el = document.createElement('img');
-        img_el.classList.add('ingredImage');
-        img_el.src = ingredients[randomIndex].image;
-
-        ingred.append(p_el);
-        ingred.append(img_el);
-
-        wrapperForBtns.append(ingred);
-
-        // Removing the ingred top item on click
-        ingred.addEventListener('click', (e) => {
-          ingred.style.display = 'none';
-          addToList(p_el, img_el);
-        });
-      }
-    }
-    const ranBtn = document.createElement('button')
-    ranBtn.classList.add('ranBtn')
-    ranBtn.innerHTML = `Generate More <img class='ranBtnImg' src='otherImages/dice.png'></img>`
-    wrapperForBtns.append(ranBtn)
-
-    generateRandomIngred(ranBtn)
-  });
-  
-}
-
-// Function to shuffle an array using the Fisher-Yates algorithm
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-
-
-// 
-
 
 function addToList(p_el, img_el) {
   const div_tag = document.createElement('div')
   div_tag.classList.add('item_el')
   const x_tag = document.createElement('p')
   x_tag.classList.add('x_tag')
-  x_tag.textContent = '\uD83D\uDDD9'
+  x_tag.innerHTML = '\uD83D\uDDD9'
   
   div_tag.append(p_el)
   div_tag.append(img_el)
@@ -116,16 +52,40 @@ function addToList(p_el, img_el) {
   chatPromptList.push(p_el.textContent)
 
   console.log(chatPromptList)
+
+  removeIngredients()
 }
 
-function removeIngredients(e) {
-  if(e.target.classList.contains('x_tag')){
-    e.target.parentElement.remove()
-  }
-  console.log(items)
+function removeIngredients() {
+  const items = document.querySelectorAll('.item_el')
+  items.forEach((item)=>{
+    item.addEventListener('click',()=>{
+      const getText = item.innerText.trim().charAt(0)+item.innerText.toLowerCase().slice(1)
+      item.remove()
+      removeFromArray(getText)
+    })
+  })
 }
+
+function removeFromArray(getText) {
+  
+  for(let i=0;i<=chatPromptList.length;i++){
+    if(chatPromptList.includes(getText)){
+      chatPromptList.pop()
+      
+      chatPromptList()
+    }
+  }
+}
+
+
+
 
 generateTopIngredients()
 
-itemsList.addEventListener('click', removeIngredients)
+
+
+
+
+
 
