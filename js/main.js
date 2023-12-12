f00a6c711a9ef990b6979d489e9ce25021e00fc2
wesclaw <108541397+wesclaw@users.noreply.github.com>
2023-12-12 19:54:11 +0100

    fixed the delete from array

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+// had to make this 2 dots for it to show on github pages. not sure why...
 import ingredients from '../database/database.js'
 
 const wrapperForBtns = document.querySelector('.wrapper-for-buttons')
 const itemsList = document.querySelector('.item-list');
+const form = document.getElementById('form')
+const input = document.getElementById('input')
 
 let chatPromptList = []
 
@@ -40,7 +43,6 @@ function addToList(p_el, img_el) {
   div_tag.classList.add('item_el')
   const x_tag = document.createElement('span')
   x_tag.classList.add('x_tag')
-  // x_tag.innerHTML = '\uD83D\uDDD9'
   x_tag.innerHTML = `<i class="fa-solid fa-xmark"></i>`
   
   div_tag.append(p_el)
@@ -49,31 +51,46 @@ function addToList(p_el, img_el) {
 
   itemsList.append(div_tag)
 
-  ///pushing the p textContent into the array for my prompt search recipes. make sure to remove the ingredients when deleted from div_tag. I will use this array for the prompt
+  ///pushing the p textContent into the array 
   chatPromptList.push(p_el.textContent)
 
-  // console.log(chatPromptList)
+  removeIngredients()
 
-  removeIngredients(div_tag, p_el, x_tag)
-
-  console.log(chatPromptList)
+  console.log(chatPromptList) ////where i can see it being added to array
 }
 
-
-function removeIngredients(div_tag, p_el, x_tag) {
-  /////i changed the p tag to a span holding the icon. now try and get the text content without getting the X. then use the include function to see if its in the chat array
+function removeIngredients() {
+  const items = document.querySelectorAll('.item_el')
+  items.forEach((item)=>{
+    item.addEventListener('click',e=>{
+      if(e.target.tagName==='I'){
+        item.remove()
+        removeFromArray(item)
+      }
+    })
+  })
 }
 
-// function removeFromArray(getText) {
-   
-// }
+function removeFromArray(item) {
+  const getText = item.textContent;
+  const index = chatPromptList.indexOf(getText);
+
+  if (index !== -1) {
+    chatPromptList.splice(index, 1); ///removing from array
+    console.log(chatPromptList); ////this is where i put the final prompt into the chat
+  }
+}
+
+function submitForm(e) {
+  e.preventDefault()
+  const inputValue = input.value;
+}
 
 
 
 generateTopIngredients()
 
-
-
+form.addEventListener('submit', submitForm)
 
 
 
