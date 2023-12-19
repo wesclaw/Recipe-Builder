9ff2efe2cc058e0cea398b6c40f3cc462aae4aa1
wesclaw <108541397+wesclaw@users.noreply.github.com>
2023-12-19 23:06:09 +0100

    sad

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const OpenAI = require('openai').OpenAI
+const openai = new OpenAI()
+
+async function main() {
+  const res = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'system',
+        content: 'you are very kind',
+      },
+      {
+        role: 'user',
+        content: 'what is your name?'
+      },
+    ],
+  })
+  console.log(res)
+}
+
+
+main()
