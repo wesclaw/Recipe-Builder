dc4755302c24929342b6c6a108278b87e6d64a25
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-01-10 20:03:47 +0100

    removed comments

--- js/main.js
+++ js/main.js
@@ -24,77 +24,21 @@ function setup() {
     },
   });
 
-  // const boxWidth = 320;
-
-  // const bowl = Bodies.rectangle(window.innerWidth / 2, window.innerHeight / 2 + 155, boxWidth, 15, { isStatic: true, render: { fillStyle: 'black' } });
-  // const rightEdge = Bodies.rectangle(window.innerWidth / 2 + boxWidth / 2, window.innerHeight / 2 + 45, 15, 250, { isStatic: true, render: { fillStyle: 'black' } });
-  // const leftEdge = Bodies.rectangle(window.innerWidth / 2 - boxWidth / 2, window.innerHeight / 2 + 45, 15, 250, { isStatic: true, render: { fillStyle: 'black' } });
-
-  // 
-
-  // const cenDiv = document.querySelector('.wrapper-for-image')
-
-  // const bowlX = cenDiv.offsetWidth / 2;
-  // const bowlY = cenDiv.offsetHeight / 2 + 445;
-  // const boxWidth = 320; // Replace with your desired box width
-  
-  // const bowl = Bodies.rectangle(bowlX, bowlY, boxWidth, 15, { isStatic: true, render: { fillStyle: 'black' } });
-  
-  // const rightEdgeX = bowlX + boxWidth / 2;
-  // const rightEdgeY = bowlY - 120; // Adjust the value based on your requirements
-  
-  // const rightEdge = Bodies.rectangle(rightEdgeX, rightEdgeY, 15, 250, { isStatic: true, render: { fillStyle: 'black' } });
-  
-  // const leftEdgeX = bowlX - boxWidth / 2;
-  // const leftEdgeY = rightEdgeY;
-  
-  // const leftEdge = Bodies.rectangle(leftEdgeX, leftEdgeY, 15, 250, { isStatic: true, render: { fillStyle: 'black' } });
-
-
-/////////////////////////////////////////////////////
-
-//   const cenDiv = document.querySelector('.wrapper-for-image');
-
-// const margin = 20; // Adjust the margin value based on your requirements
-// const boxWidth = 320; // Replace with your desired box width
-
-// const bowlX = cenDiv.offsetWidth / 2;
-// const bowlY = cenDiv.offsetHeight / 2 + 445 + margin; // Adjusted to add margin
-// const rightEdgeX = bowlX + boxWidth / 2;
-// const rightEdgeY = bowlY - 120 + margin; // Adjusted to add margin
-// const leftEdgeX = bowlX - boxWidth / 2;
-// const leftEdgeY = rightEdgeY;
-
-// const bowl = Bodies.rectangle(bowlX, bowlY, boxWidth, 15, { isStatic: true, render: { fillStyle: 'black' } });
-// const rightEdge = Bodies.rectangle(rightEdgeX, rightEdgeY, 15, 250, { isStatic: true, render: { fillStyle: 'black' } });
-// const leftEdge = Bodies.rectangle(leftEdgeX, leftEdgeY, 15, 250, { isStatic: true, render: { fillStyle: 'black' } });
-
-
-
-  // 
-
   const cenDiv = document.querySelector('.wrapper-for-image');
 
-  const margin = 50; // Adjust the margin value based on your requirements
-  const boxWidth = 240; // Replace with your desired box width
+  const margin = 50;
+  const boxWidth = 240;
   
   const bowlX = cenDiv.offsetWidth / 2;
-  const bowlY = cenDiv.offsetHeight / 2 + 415 + margin; // Adjusted to add margin
+  const bowlY = cenDiv.offsetHeight / 2 + 415 + margin; 
   const rightEdgeX = bowlX + boxWidth / 2;
-  const rightEdgeY = bowlY - 120 + margin; // Adjusted to add margin
+  const rightEdgeY = bowlY - 120 + margin; 
   const leftEdgeX = bowlX - boxWidth / 2;
   const leftEdgeY = rightEdgeY;
   
   const bowl = Bodies.rectangle(bowlX, bowlY, boxWidth, 15, { isStatic: true, render: { fillStyle: 'transparent' } });
   const rightEdge = Bodies.rectangle(rightEdgeX, rightEdgeY, 15, 220, { isStatic: true, render: { fillStyle: 'transparent' } });
   const leftEdge = Bodies.rectangle(leftEdgeX, leftEdgeY, 15, 220, { isStatic: true, render: { fillStyle: 'transparent' } });
-
-
-  // 
-
-
-
-
 
   World.add(engine.world, [bowl, rightEdge, leftEdge]);
 
@@ -130,28 +74,6 @@ function sendArrayData() {
 ws.onmessage = (event) => {  
   const responseData = event.data
 
-  // const body_el = document.querySelector('body')
-  
-  // const module_outer = document.createElement('div')
-  // module_outer.classList.add('module-outer')
-  
-  // const new_item_list = document.createElement('div')
-  // new_item_list.classList.add('new-item-list')
- 
-  // const i_tag = document.createElement('i')
-  // i_tag.classList.add('fa-solid', 'fa-xmark', 'x-mark')
-
-  // const pre_tag = document.createElement('pre')
-
-  // body_el.append(module_outer)
-  // module_outer.append(new_item_list)
-  // new_item_list.append(i_tag)
-  // new_item_list.append(pre_tag)
-
-  // pre_tag.textContent = responseData
-
-  /////////////////
-
   itemsList.innerHTML = `
   <div class='module-outer'>
     <div class='new-item-list'>
@@ -163,7 +85,6 @@ ws.onmessage = (event) => {
   window.addEventListener('click',e=>{
     if(e.target.tagName==='I'){
       window.location.reload()
-      ////maybe just remove the parent element instead of page refresh?////why reload the whole page? it will tale extra longer
     }
   })
 };
@@ -192,15 +113,8 @@ function generateTopIngredients(ingred) {
 
       createPear(p_el)
     })
-  }
-  ///making the generate more btn/////
-    // const ranBtn = document.createElement('button')
-    // ranBtn.classList.add('ranBtn')
-    // ranBtn.innerHTML = `Generate More <img class='ranBtnImg' src='otherImages/dice.png'></img>`
-    // wrapperForBtns.insertBefore(ranBtn, ingred)   
+  }  
 }
-
-///////////////////////////////////////////////////
 
 let foodArray = new Map()
 
@@ -212,7 +126,6 @@ function createPear(p_el) {
 
   pearImage.src = 'images/' + p_text + '.png';
 
-  // Create a fallingPear variable
   let fallingPear;
 
   pearImage.onload = function () {
@@ -232,13 +145,10 @@ function createPear(p_el) {
       friction: 0.7,
     });
 
-    // Add fallingPear to the Matter.js world
     World.add(engine.world, fallingPear);
 
-    // Add both p_text and fallingPear to the foodArray Map
     foodArray.set(p_text, fallingPear);
 
-    // Assuming 'render' is the Render object created with Matter.js
     Render.lookAt(render, {
       min: { x: 0, y: 0 },
       max: { x: window.innerWidth, y: window.innerHeight },
@@ -249,24 +159,17 @@ function createPear(p_el) {
     console.error('Error loading image:', p_el, error);
   };
 
-  // Return both pearImage and fallingPear
   return { pearImage, fallingPear };
 }
 
 function removeTheFood(getText) {
-  // Convert getText to lowercase
   const lowerCaseText = getText.toLowerCase();
-  // Check if the lowercased text is in the foodArray Map
   if (foodArray.has(lowerCaseText)) {
-    // Retrieve the fallingPear from the foodArray Map
     const fallingPear = foodArray.get(lowerCaseText);
 
-    // Remove the fallingPear from the Matter.js world
     World.remove(engine.world, fallingPear);
 
     foodArray.delete(lowerCaseText);
-
-    console.log(`Removed fallingPear associated with ${lowerCaseText}`);
   } else {
     console.log(`${lowerCaseText} not found in the foodArray Map.`);
   }
@@ -289,9 +192,6 @@ function addToList(p_el, img_el) {
   chatPromptList.push(p_el.textContent)
 
   removeIngredients()
-
-  console.log(chatPromptList) ////where i can see it being added to array
-  // 
   disableAnEnableBtn()
 }
 
@@ -358,7 +258,6 @@ function submitForm(e) {
       img_el.src = ingredients[i].image;
     }
   }
-    /////////////////////////////
   const x_tag = document.createElement('span')
   x_tag.classList.add('x_tag')
   x_tag.innerHTML = `<i class="fa-solid fa-xmark"></i>`
@@ -387,19 +286,6 @@ generateTopIngredients()
 form.addEventListener('submit', submitForm)
 
 recipeBtn.addEventListener('click', sendArrayData)
-
-
-
-// function removeItem(item) {
-//   const index = chatPromptList.indexOf(item);
-//   if (index !== -1) {
-//     chatPromptList.splice(index, 1);
-//     sendArrayData();
-//   }
-// }
-
-//
-
 
 
 
